846c297aea8925f0357c059341a086c8b7520b2d
Manaswini-mahala <mahalamanaswini@gmail.com>
2023-01-16 21:41:55 +0530

    rectified the nominate now button & fixed About ACS rockstars scroll

--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -5,7 +5,7 @@
             var hash = this.hash;
             $('html, body').animate({
               scrollTop: $(hash).offset().top
-            }, 1000, function(){
+            }, 750, function(){
               window.location.hash = hash;
             });
           }
