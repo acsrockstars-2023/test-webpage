c569490a3199b8738a70af34ac8c9eaf16f144cb
Chethan Kumar <chethankuma@adobe.com>
2023-01-18 12:31:06 +0530

    Code refector

--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -1,16 +1,15 @@
-
-        $("#navbar-scroll-acs").on('click', function(event){
-          if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-              scrollTop: $(hash).offset().top
-            }, 750, function(){
-              window.location.hash = hash;
-            });
-          }
-        });
-
-
-
-    
+$("#navbar-scroll-acs").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      750,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
